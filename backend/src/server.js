50a8e3dc7e362efccf36c40451e41bbe3d541ba1
Abhishek <abhishekabhishek1208@gmail.com>
2026-06-10 13:49:12 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
- 
+ import path from 'path';
 const app = express();
 
+const __dirname = path.resolve();
 app.get('/health', (req, res) => {
-  res.json({ message: 'api is up and running' });
+  res.status(200).json({ message: 'api is up and running' });
+});
+app.get('/books', (req, res) => {
+  res.status(200).json({ message: 'this is books endpoint' });
 });
  
+// make raedy for deployment
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend','dist','index.html'));
+  });
+}
+
+
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port:${ENV.PORT}`);
 });
