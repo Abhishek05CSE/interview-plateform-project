8e6532634c66fd1eb82e3691795d2b4c9409a444
Abhishek <abhishekabhishek1208@gmail.com>
2026-06-10 12:55:34 +0530

    initial commit

---
+++ backend/src/lib/env.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const ENV = {
+    PORT: process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/myapp',
+    
+}
+
+console.log(process.env.DB_URL);
