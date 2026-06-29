bf8f1b8cc96947f70a636704ab69ff84ea4a9faf
Abhishek <abhishekabhishek1208@gmail.com>
2026-06-29 13:59:12 +0530

    auth and db sync

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,7 +4,13 @@ dotenv.config();
 export const ENV = {
     PORT: process.env.PORT || 3000,
     DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/myapp',
-    NODE_ENV: process.env.NODE_ENV || 'development'
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
+    INNGEST_EVENT_KEY:process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_API_SECRET:process.env.STREAM_API_SECRET,
+
 }
 
 console.log(process.env.DB_URL);
